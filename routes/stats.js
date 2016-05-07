cec3e854d603fdd3bc5a099fef03749e74830abb
Daria Mikhailova <dariam89@mail.ru>
2016-05-07 11:27:19 +0200

    endpoint for statistics

--- routes/stats.js
+++ routes/stats.js
@@ -3,14 +3,35 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/countries', function (req, res) {
-    Message.aggregate({$group: {country: '$country', city: '$city'}}, function (err, result) {
+    Message.aggregate([
+        {$group: {_id: "$country", value: {$sum: 1}}},
+        {$project: {_id: 0, x: "$_id", y: "$value"}},
+        {$sort: {y: -1}},
+        {$limit: 10}
+    ], function (err, result) {
         if (err) {
             console.log(err);
             return res.status(400).json(err);
         }
         if (res) {
-            console.log(result);
-            return res.status(200).json(result);
+            return res.status(200).json({values: result});
+        }
+    })
+});
+
+router.get('/cities', function (req, res) {
+    Message.aggregate([
+        {$group: {_id: "$city", value: {$sum: 1}}},
+        {$project: {_id: 0, x: "$_id", y: "$value"}},
+        {$sort: {y: -1}},
+        {$limit: 10}
+    ], function (err, result) {
+        if (err) {
+            console.log(err);
+            return res.status(400).json(err);
+        }
+        if (res) {
+            return res.status(200).json({values: result});
         }
     })
 });
